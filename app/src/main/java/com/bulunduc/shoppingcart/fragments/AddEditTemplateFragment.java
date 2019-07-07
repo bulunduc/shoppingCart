ef61f238aad0c3260258f416ee7ee1d85d8a225e
v.krivosheeva <v.krivosheeva@corp.mail.ru>
2019-07-07 23:20:32 +0300

    cosmetic fixes

--- app/src/main/java/com/bulunduc/shoppingcart/fragments/AddEditTemplateFragment.java
+++ app/src/main/java/com/bulunduc/shoppingcart/fragments/AddEditTemplateFragment.java
@@ -9,9 +9,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.bulunduc.shoppingcart.R;
 import com.bulunduc.shoppingcart.adapters.AddTemplateProductsAdapter;
+import com.bulunduc.shoppingcart.filters.NumberInputFilter;
 import com.bulunduc.shoppingcart.models.Item;
 
 import java.util.ArrayList;
@@ -20,31 +24,47 @@ public class AddEditTemplateFragment extends DialogFragment {
 
     private static final String TAG = "ShowTemplateProductsFragment";
     private static Context mContext;
-    RecyclerView mProductsList;
+    RecyclerView rvProductsList;
     AddTemplateProductsAdapter adapter;
     private static ArrayList<Item> mProducts = new ArrayList<>();
     private static String mTemplateTitle = "title";
+    private EditText etTitle;
+    private EditText etCount;
+    private EditText etPrice;
+    private Spinner spUnit;
+    private Button btnAddProduct;
 
-    public static AddEditTemplateFragment newInstance(Context context){
+    public static AddEditTemplateFragment newInstance(Context context) {
         mContext = context;
         return new AddEditTemplateFragment();
     }
 
-    public static AddEditTemplateFragment newInstance(Context context, String title, ArrayList<Item> products){
+    public static AddEditTemplateFragment newInstance(Context context, String title, ArrayList<Item> products) {
         mContext = context;
         mProducts = products;
         mTemplateTitle = title;
         return new AddEditTemplateFragment();
     }
 
+    private void initView(View rootView) {
+        rvProductsList = rootView.findViewById(R.id.rvTemplProducts);
+
+        etTitle = rootView.findViewById(R.id.etTemplProdTitle);
+        etCount = rootView.findViewById(R.id.etTemplProdCount);
+        spUnit = rootView.findViewById(R.id.spTemplProdUnit);
+        etPrice = rootView.findViewById(R.id.etTemplProdPrice);
+        btnAddProduct = rootView.findViewById(R.id.btnTemplAddProduct);
+        etCount.setFilters(new NumberInputFilter[]{new NumberInputFilter(6, 2)});
+        etPrice.setFilters(new NumberInputFilter[]{new NumberInputFilter(6, 2)});
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View rootView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_add_template, null);
-        mProductsList = rootView.findViewById(R.id.rvProducts);
-        mProductsList.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-
+        initView(rootView);
+        rvProductsList.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         adapter = new AddTemplateProductsAdapter(this.getActivity(), mProducts);
-        mProductsList.setAdapter(adapter);
+        rvProductsList.setAdapter(adapter);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
         builder.setView(rootView)
