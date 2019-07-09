d290578f713438dedde592b02d462af360256f6b
v.krivosheeva <v.krivosheeva@corp.mail.ru>
2019-07-09 14:27:28 +0300

    templates: delete and edit, cosmetic fixes

--- app/src/main/java/com/bulunduc/shoppingcart/activity/TemplatesActivity.java
+++ app/src/main/java/com/bulunduc/shoppingcart/activity/TemplatesActivity.java
@@ -1,33 +1,29 @@
 package com.bulunduc.shoppingcart.activity;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.bulunduc.shoppingcart.R;
 import com.bulunduc.shoppingcart.adapters.TemplateAdapter;
-import com.bulunduc.shoppingcart.constants.AppConstants;
 import com.bulunduc.shoppingcart.fragments.AddEditTemplateFragment;
-import com.bulunduc.shoppingcart.models.CartItem;
+import com.bulunduc.shoppingcart.listeners.TemplateDialogClickListener;
 import com.bulunduc.shoppingcart.models.Template;
 import com.bulunduc.shoppingcart.utilities.AppUtilities;
 
 import java.util.ArrayList;
 
-public class TemplatesActivity extends BaseActivity {
+public class TemplatesActivity extends BaseActivity implements TemplateDialogClickListener {
     private RecyclerView rvTemplates;
     private FloatingActionButton mFloatingActionButton;
     private TemplateAdapter adapter = null;
-    private ArrayList<Template> templates;
+    private ArrayList<Template> mTemplates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +35,11 @@ public class TemplatesActivity extends BaseActivity {
     private void initView(){
         rvTemplates = findViewById(R.id.rvTemplates);
         rvTemplates.setLayoutManager(new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false));
-        templates = AppUtilities.getTemplatesList(this.getApplicationContext());
-        adapter = new TemplateAdapter(this.getApplicationContext(), this, templates);
+        mTemplates = AppUtilities.getTemplatesList(this.getApplicationContext());
+        adapter = new TemplateAdapter(this.getApplicationContext(), this, mTemplates);
         rvTemplates.setAdapter(adapter);
         mFloatingActionButton = findViewById(R.id.addFab);
         mFloatingActionButton.setOnClickListener(v -> {
-            Bundle args = new Bundle();
             AddEditTemplateFragment fragment = new AddEditTemplateFragment();
             fragment.show(this.getFragmentManager(), fragment.getTag());
         });
@@ -70,4 +65,28 @@ public class TemplatesActivity extends BaseActivity {
             return false;
         });
     }
+
+    @Override
+    public void onTemplateAddClick(Template template, int position) {
+        if (position < 0) {
+            mTemplates.add(template);
+        } else {
+            mTemplates.set(position, template);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onTemplateDeleteClick(int position) {
+        mTemplates.remove(position); //TODO add confirming dialog
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppUtilities.saveTemplatesList(getApplicationContext(), mTemplates);
+    }
+
+
 }
